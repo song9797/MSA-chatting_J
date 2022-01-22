c8527067503f4a65f514373c6df80b5c2b3a95ed
zzzinho <hahoh0013@gmail.com>
2022-01-22 12:48:52 +0900

    config

---
+++ confsvr/src/main/java/com/example/confsvr/ConfsvrApplication.java
@@ -0,0 +1,15 @@
+package com.example.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfsvrApplication.class, args);
+	}
+
+}
